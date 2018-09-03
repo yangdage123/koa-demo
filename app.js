2eb51029619482d85c2ec4ed8aec3af1fdb9ff96
yangdage <515411030@qq.com>
2018-09-03 18:40:10 +0800

    koa demo开发,添加了数据库的 rollback操作

---
+++ app.js
@@ -0,0 +1,94 @@
+const Koa = require('koa');
+const _ = require('koa-route');
+const ser = require('koa-static');
+const path = require('path');
+const app = new Koa();
+const DAO = require('./modules/DAO');
+
+const main = ser(path.join(__dirname, 'static'));
+
+const handler = async (e, ctx) => {
+  const {
+    response,
+    request,
+  } = ctx;
+  response.status = e.statusCode || e.status || 500;
+  response.body = {
+    message: e.message
+  };
+  console.error(`[error][${Date.now()}] ${request.method} ${request.url} [reason] ${e.message} `);
+};
+const logger = async (ctx, next) => {
+  const {
+    request,
+  } = ctx;
+  console.log(`[info] [${Date.now()}] ${request.method} ${request.url} >>`);
+  await next();
+  console.log(`[info] [${Date.now()}] ${request.method} ${request.url} <<`)
+};
+
+const root = (ctx) => {
+  const {
+    response,
+  } = ctx;
+  response.type = 'html';
+  response.body = '<div>root</div>';
+};
+
+const about = (ctx) => {
+  const {
+    response,
+  } = ctx;
+  response.type = 'html';
+  response.body = '<div>about</div>';
+};
+
+const error = (ctx) => {
+  throw new Error('shibai');
+};
+
+const getData = async (ctx) => {
+  const {
+    response,
+  } = ctx;
+  const rows = await DAO.getData();
+  response.type = 'json';
+  response.body = {
+    code: 0,
+    info: {
+      list: rows,
+    },
+  msg: 'success',
+  }
+};
+
+const add = async (ctx) => {
+  const {
+    response,
+  } = ctx;
+  const rows = await DAO.add();
+  response.type = 'json';
+  response.body = {
+    code: 0,
+    info: {
+    },
+    msg: 'success',
+  }
+};
+
+// app.use(handler);
+app.use(main);
+app.use(logger);
+app.use(_.get('/', root));
+app.use(_.get('/about', about));
+app.use(_.get('/error', error));
+app.use(_.get('/getData', getData));
+app.use(_.get('/add', add));
+
+app.on('error', handler);
+
+app.listen(3000);
+console.log('server listen 3000...');
+
+
+
